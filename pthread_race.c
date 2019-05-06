da4541a8d01deff87175dfa627a38dce26e9fe58
Imcray <roarkyo@gmail.com>
2019-05-06 00:21:10 -0700

    Added mutex syncronization calls

--- pthread_race.c
+++ pthread_race.c
@@ -22,22 +22,16 @@
  */
 struct timespec ts = {0, 150000};
 static int sharedVar = 0;
-pthread_mutex_t mutex;
-
-
 
 void *adder(void *arg) {
 	int temp;
 	int tid = *((int*)arg);
 	for (int i=0; i < MAX_ITERATIONS; i++){
-		pthread_mutex_lock(&mutex);
-	/* Critical Section */	
+		
 		temp = sharedVar;
 		nanosleep(&ts, NULL);
 		temp = temp + 10;
 		sharedVar = temp;
-	/* End Critical Section */
-		pthread_mutex_unlock(&mutex);
 		printf("Current Value written to Global Variables by ADDER thread id: %d is %d\n", tid, temp);
 	}
 }
@@ -46,13 +40,9 @@ void *subtractor(void *arg) {
 	int temp;
 	int tid = *((int*)arg);
 	for (int i=0; i < MAX_ITERATIONS; i++){
-		pthread_mutex_lock(&mutex);
-	/* Critical Section */
 		temp = sharedVar;
 		temp = temp - 10;
 		sharedVar = temp;
-	/* End Critical Section */
-		pthread_mutex_unlock(&mutex);
 		printf("Current Value written to Global Variables by SUBTRACTOR thread id: %d is %d\n", tid, temp);
 	}
 }
@@ -60,14 +50,7 @@ void *subtractor(void *arg) {
 int
 main(int argc, char** argv)
 {	
-	
-	
 	pthread_t tid[MAX_THREADS];
-	
-	
-	/*create and init the mutex lock */
-	pthread_mutex_init(&mutex, NULL);
-	
 	for (int i = 0; i < MAX_THREADS; i++){
 		if (i%2 == 0){
 			pthread_create(&tid[i], NULL, adder, (void *)&i);
